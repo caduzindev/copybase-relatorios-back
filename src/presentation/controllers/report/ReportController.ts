5a13fcc5c4d66345a6dd52ab632031a308c84106
carlos eduardo <cadudevmaster@gmail.com>
2024-01-08 22:38:06 -0300

    feat: send file

--- src/presentation/controllers/report/ReportController.ts
+++ src/presentation/controllers/report/ReportController.ts
@@ -6,12 +6,11 @@ export class ReportController implements IReportController {
     constructor(private readonly reportUseCase: IReportUseCase) {}
     async requestReport(request: HttpRequestDTO): Promise<HttpResponseDTO> {
         try {
-            console.log(request);
-            // const filePath = request.body.filePath;
-            // await this.reportUseCase.requestReport(filePath);
+            const filePath = request.file?.path as string;
+            await this.reportUseCase.requestReport(filePath);
             return {
                 statusCode: 200,
-                body: ''
+                body: 'Arquivo enviado com sucesso!'
             }
         } catch(error: any) {
             console.log(error)
